51d3b972d214b33361db4474b64e430ebad9e5b2
Alexey Bondarenko <alexeybondarenko@me.com>
2015-02-18 22:52:13 +0200

    Initial Commit

---
+++ Thread1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Thread1
+//
+//  Created by Alexey Bondarenko on 2/18/15.
+//  Copyright (c) 2015 Alexey Bondarenko. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
